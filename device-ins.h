21786dd53d6167755a9dd210f4dee1281c78e59d
Samy Bencherif <samybencherif@live.com>
2019-07-10 15:38:08 -0400

    compiles with notes

--- device-ins.h
+++ device-ins.h
@@ -14,13 +14,28 @@ feedback devices_send_ins(instruction i)
       printf("TRACE: SET INS <devid: %d> <signal: %d>\n", i->dev_id,
           i->signal);
 
+      assert(i->signal == ON || i->signal == OFF);
+      /* TODO set hardware.
+       *
+       * i->dev_id is the device identifier
+       * i->signal is the signal to be set (ON or OFF)
+       */
+
       /*
        * Trade study:
        * Should there be an implicit ENSURE after every SET?
-       *
-       * */
+       */
 
       return SUCCESS;
+
+    case WAIT:
+      /* TODO wait */
+      return SUCCESS;
+
+    case ENSURE:
+      assert(0);
+
+      return FAIL;
 
     default:
       fprintf(stderr, "ERROR: INS VALUE %d UNIMPLEMENTED.\n", i->ins_mode);
